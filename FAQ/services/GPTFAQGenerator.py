0abaa63c46684bc5e5ca18a3d1ea9f0b6aa9f00f
DAGASHY\zseba <zsebastien@yahoo.fr>
2025-06-17 14:34:18 +0200

    move FAQGenerator et GPTFAQGenerator vers services

---
+++ FAQ/services/GPTFAQGenerator.py
@@ -0,0 +1,8 @@
+from .FAQGenerator import FAQGenerator
+
+class GPTFAQGenerator(FAQGenerator):
+    """
+    A class to generate FAQs using a GPT model.
+    Inherits from BaseFaqGenerator.
+    """
+    pass
