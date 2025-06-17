d5bf623a8ffdc7e11043faf466d3132cddeb5e81
DAGASHY\zseba <zsebastien@yahoo.fr>
2025-06-17 11:49:49 +0200

    ajout du model History

---
+++ FAQ/models/history.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class History(models.Model):
+
+    name = models.CharField(max_length=255)
+    #à discuter
+    details = models.TextField()
+    #c'est l'url visitée ?
+    faq_id = models.ForeignKey('faq.Faq', on_delete=models.CASCADE, related_name='histories')
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.faq_id.question} by {self.created_by.username} on {self.created_at.strftime('%Y-%m-%d %H:%M:%S')}"
+        #autocompleted, je ne sais pas encore ce qu'on veux pouvoir récup de l'historique.
+
