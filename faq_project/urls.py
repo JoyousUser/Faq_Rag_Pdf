c829e0a34b0392ed7ee94d452f06a33167eef2a4
AbdoulAlim idlbi <abdoulalim.idlbi@gmail.com>
2025-06-19 13:44:55 +0200

    chore(urls): updated global urls to import api endpoints

--- faq_project/urls.py
+++ faq_project/urls.py
@@ -16,17 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import routers
-from FAQ.views import UserViewSet
 
-router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('FAQ.urls')),
-    path('api/', include(router.urls)),
 
 ]
