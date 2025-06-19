3aea3826541e78aaf0fbbc5a789060711b17f6d6
AbdoulAlim idlbi <abdoulalim.idlbi@gmail.com>
2025-06-19 13:42:24 +0200

    chore(endpoints): API endpoints, urls & serializers

--- FAQ/serializers.py
+++ FAQ/serializers.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from .models import Faq, UploadedFiles, History
 from rest_framework import serializers
 
 
@@ -6,3 +7,26 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User    
         fields = ['id', 'url', 'username', 'first_name', 'last_name', 'email', 'is_staff']
+
+class FaqSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Faq
+        fields = ['id', 'url', 'author', 'created_at', 'updated_at', 'question', 'answer', 'generation']
+    # json example of an FAQ
+    # {
+    #     "id": 1,
+    #     "author": 1,
+    #     "question": "What is Django?",
+    #     "answer": "Django is a high-level Python Web framework that encourages rapid development and clean, pragmatic design.",
+    #     "generation": "Manual"
+    # }
+
+class UploadedFilesSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = UploadedFiles
+        fields = ['id', 'url', 'file_path', 'created_at', 'created_by']
+
+class HistorySerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = History
+        fields = ['visisted', 'faq_id', 'visited_by', 'created_at']
--- FAQ/urls.py
+++ FAQ/urls.py
@@ -1,8 +1,17 @@
-from django.urls import path
-from .views import LoginView, UserViewSet, LogoutView
+from django.urls import path, include
+from .views import LoginView, UserViewSet, LogoutView, FaqViewSet, UploadedFilesViewSet
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'faqs', FaqViewSet)
+router.register(r'uploadedfiles', UploadedFilesViewSet)
+
 
 urlpatterns = [
-    path('users/', UserViewSet.as_view({'get': 'list'})),
     path('login/', LoginView.as_view()),
-    path('logout/', LogoutView.as_view())
+    path('logout/', LogoutView.as_view()),
+    # path('users/', UserViewSet.as_view({'get': 'list'})),
+    # path('faqs/', FaqViewSet.as_view({'get': 'list', 'post': 'create'})),
+    path('', include(router.urls)),
 ]
