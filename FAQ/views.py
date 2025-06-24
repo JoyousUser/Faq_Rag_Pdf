b1853ec1a7709318bcfedeeebeddb30415acc806
AbdoulAlim idlbi <abdoulalim.idlbi@gmail.com>
2025-06-24 14:53:05 +0200

    chore(views): updated permissions

--- FAQ/views.py
+++ FAQ/views.py
@@ -45,30 +45,52 @@ class FaqViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows faqs to be viewed or edited.
     """
-    permission_classes = [permissions.IsAuthenticated]
     authentication_classes = [authentication.TokenAuthentication]
     queryset = Faq.objects.all()
     serializer_class = FaqSerializer
 
-    def create(self, request):
+    def get_permissions(self):
+        """
+        Instantiates and returns the list of permissions that this view requires.
+        """
+        if self.action == 'create':
+            permission_classes = [permissions.IsAuthenticated, permissions.IsAdminUser]
+        elif self.action in ['list', 'retrieve']:
+            permission_classes = [permissions.AllowAny]
+        else:
+            permission_classes = [permissions.IsAuthenticated, permissions.IsAdminUser]
+        return [permission() for permission in permission_classes]
+
+    def perform_create(self, request):
         user = request.user
         faq = Faq.objects.create(author=user, question=request.data.get('question'), answer=request.data.get('answer'), generation=request.data.get('generation'))
         serializer = FaqSerializer(faq, context={'request': request})
         return Response(serializer.data, status=status.HTTP_201_CREATED)
-    
-    def list(self, request):
-        faqs = Faq.objects.all()
-        serializer = FaqSerializer(faqs, many=True, context={'request': request})
-        return Response(serializer.data)
     
 
 class UploadedFilesViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows faqs to be viewed or edited.
     """
-    permission_classes = [permissions.IsAuthenticated]
     authentication_classes = [authentication.TokenAuthentication]
     queryset = UploadedFiles.objects.all()
     serializer_class = UploadedFilesSerializer
 
+    def get_permissions(self):
+        """
+        Instantiates and returns the list of permissions that this view requires.
+        """
+        if self.action == 'create':
+            permission_classes = [permissions.IsAuthenticated, permissions.IsAdminUser]
+        elif self.action in ['list', 'retrieve']:
+            permission_classes = [permissions.AllowAny]
+        else:
+            permission_classes = [permissions.IsAuthenticated, permissions.IsAdminUser]
+        return [permission() for permission in permission_classes]
+
+    def perform_create(self, request):
+        user = request.user
+        faq = Faq.objects.create(author=user, question=request.data.get('question'), answer=request.data.get('answer'), generation=request.data.get('generation'))
+        serializer = FaqSerializer(faq, context={'request': request})
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
