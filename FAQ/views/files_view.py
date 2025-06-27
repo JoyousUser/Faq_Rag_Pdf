dcb152e5d18b12b1e9cbfe044dca53e6c7e70342
AbdoulAlim idlbi <abdoulalim.idlbi@gmail.com>
2025-06-27 16:00:01 +0200

    fix(files uploader): fixed files not uploading through API

--- FAQ/views/files_view.py
+++ FAQ/views/files_view.py
@@ -1,12 +1,14 @@
 from rest_framework import viewsets, permissions, authentication, status
-from ..models import UploadedFiles, Faq
-from ..serializers import UploadedFilesSerializer, FaqSerializer
+from rest_framework.parsers import MultiPartParser, FormParser
+from ..models import UploadedFiles
+from ..serializers import UploadedFilesSerializer
 from rest_framework.response import Response
 
 class UploadedFilesViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows faqs to be viewed or edited.
     """
+    parser_classes = [MultiPartParser, FormParser]
     authentication_classes = [authentication.TokenAuthentication]
     queryset = UploadedFiles.objects.all()
     serializer_class = UploadedFilesSerializer
@@ -17,6 +19,7 @@ class UploadedFilesViewSet(viewsets.ModelViewSet):
         """
         if self.action == 'create':
             permission_classes = [permissions.IsAuthenticated, permissions.IsAdminUser]
+
         elif self.action in ['list', 'retrieve']:
             permission_classes = [permissions.AllowAny]
         else:
@@ -25,7 +28,12 @@ class UploadedFilesViewSet(viewsets.ModelViewSet):
     
     def perform_create(self, request):
         user = request.user
-        faq = Faq.objects.create(author=user, question=request.data.get('question'), answer=request.data.get('answer'), generation=request.data.get('generation'))
-        serializer = FaqSerializer(faq, context={'request': request})
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        uploaded_file = request.FILES.get('file_path')
+        if not uploaded_file:
+            return Response({'error': 'No file provided'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        file = UploadedFiles.objects.create(file_path=uploaded_file, created_by=user)
+        serializer = UploadedFilesSerializer(file, context={'request': request})
+         
+        return Response(status=status.HTTP_201_CREATED)
 
