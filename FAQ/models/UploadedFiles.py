e3431fa80d719012cf7803ba46751a9aca37338b
AbdoulAlim idlbi <abdoulalim.idlbi@gmail.com>
2025-07-15 16:02:40 +0200

    fix(relation): removed unused ManyToOne relation

--- FAQ/models/UploadedFiles.py
+++ FAQ/models/UploadedFiles.py
@@ -7,7 +7,6 @@ class UploadedFiles(models.Model):
     #upload le fichier vers le dossier MEDIA_ROOT/uploaded_files/ et enregistre son chemin relatif en bdd
     created_at = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
-    faqs = models.ManyToOneRel('FAQ.Faq', 'Faq', related_name='uploaded_files')
 
     def __str__(self):
         return f"File: {self.file_path.name} uploaded by {self.created_by.username} on {self.created_at.strftime('%Y-%m-%d %H:%M:%S')}"
