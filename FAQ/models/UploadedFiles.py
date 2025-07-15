68ed726ce5cee7811df3c18ea485f46db59f8cc3
AbdoulAlim idlbi <abdoulalim.idlbi@gmail.com>
2025-07-15 15:55:40 +0200

    feat(file_name): added new file name field to UploadedFiles

--- FAQ/models/UploadedFiles.py
+++ FAQ/models/UploadedFiles.py
@@ -2,11 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class UploadedFiles(models.Model):
-
+    file_name = models.CharField(max_length=255, default='Not provided')
     file_path = models.FileField(upload_to='uploaded_files/')
     #upload le fichier vers le dossier MEDIA_ROOT/uploaded_files/ et enregistre son chemin relatif en bdd
     created_at = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    faqs = models.ManyToOneRel('FAQ.Faq', 'Faq', related_name='uploaded_files')
 
     def __str__(self):
         return f"File: {self.file_path.name} uploaded by {self.created_by.username} on {self.created_at.strftime('%Y-%m-%d %H:%M:%S')}"
